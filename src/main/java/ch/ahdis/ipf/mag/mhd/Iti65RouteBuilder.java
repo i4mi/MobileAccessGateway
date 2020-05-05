152cec2ee6a0f669e1f56091761181bb0e062796
Oliver Egger <oliver.egger@ahdis.ch>
2020-05-05 21:15:55 +0200

    ITI 65 mock

---
+++ src/main/java/ch/ahdis/ipf/mag/mhd/Iti65RouteBuilder.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ch.ahdis.ipf.mag.mhd;
+
+import java.util.Date;
+import java.util.UUID;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.support.ExpressionAdapter;
+import org.hl7.fhir.r4.model.Binary;
+import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.DocumentManifest;
+import org.hl7.fhir.r4.model.DocumentReference;
+import org.hl7.fhir.r4.model.ListResource;
+import org.hl7.fhir.r4.model.Resource;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * IHE MHD: [ITI-65] Provide Document Bundle Request Message for Document Recipient
+ * https://oehf.github.io/ipf-docs/docs/ihe/iti65/
+ */
+@Slf4j
+@Component
+class Iti65RouteBuilder extends RouteBuilder {
+
+    public Iti65RouteBuilder() {
+        super();
+        log.debug("Iti65RouteBuilder initialized");
+    }
+
+    @Override
+    public void configure() throws Exception {
+        log.debug("Iti65RouteBuilder configure");
+        from("mhd-iti65:stub?audit=false&fhirContext=#fhirContext").routeId("mdh-providedocumentbundle")
+                // pass back errors to the endpoint
+                .errorHandler(noErrorHandler())
+                // translate, forward, translate back
+                .transform(new Responder());
+    }
+
+    private class Responder extends ExpressionAdapter {
+
+        @Override
+        public Object evaluate(Exchange exchange) {
+            Bundle requestBundle = exchange.getIn().getBody(Bundle.class);
+
+            Bundle responseBundle = new Bundle()
+                    .setType(Bundle.BundleType.TRANSACTIONRESPONSE)
+                    .setTotal(requestBundle.getTotal());
+
+            for (Bundle.BundleEntryComponent requestEntry : requestBundle.getEntry()) {
+                Bundle.BundleEntryResponseComponent response = new Bundle.BundleEntryResponseComponent()
+                        .setStatus("201 Created")
+                        .setLastModified(new Date())
+                        .setLocation(requestEntry.getResource().getClass().getSimpleName() + "/" + 4711);
+                responseBundle.addEntry()
+                        .setResponse(response)
+                        .setResource(responseResource(requestEntry.getResource()));
+            }
+            return responseBundle;
+        }
+
+    }
+
+    private Resource responseResource(Resource request) {
+        if (request instanceof DocumentManifest) {
+            return new DocumentManifest().setId(UUID.randomUUID().toString());
+        } else if (request instanceof DocumentReference) {
+            return new DocumentReference().setId(UUID.randomUUID().toString());
+        } else if (request instanceof ListResource) {
+            return new ListResource().setId(UUID.randomUUID().toString());
+        } else if (request instanceof Binary) {
+            return new Binary().setId(UUID.randomUUID().toString());
+        } else {
+            throw new IllegalArgumentException(request + " is not allowed here");
+        }
+    }
+}
