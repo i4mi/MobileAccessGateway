1c08c2c7842aaead63842fc6d65e83552f305265
Alexander-Kreutz <kreutz@i-tv-t.de>
2021-08-31 10:54:54 +0200

    fix assertion requests

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p secret
 cp $1/* secret
-mvn compile exec:java -Dspring.config.additional-location=file:$1/application.yml
+mvn clean compile exec:java -Dspring.config.additional-location=file:$1/application.yml
 rm -f secret/*
 rmdir secret
