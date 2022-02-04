abcf430c6a616c98d5ec83de3c92dec1e37848e3
Alexander-Kreutz <kreutz@i-tv-t.de>
2022-02-04 12:52:32 +0100

    update for auth

---
+++ src/main/java/ch/bfh/ti/i4mi/mag/xua/MySAMLBootstrap.java
@@ -0,0 +1,19 @@
+package ch.bfh.ti.i4mi.mag.xua;
+
+import org.opensaml.Configuration;
+import org.opensaml.xml.security.BasicSecurityConfiguration;
+import org.opensaml.xml.signature.SignatureConstants;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.security.saml.SAMLBootstrap;
+
+public class MySAMLBootstrap extends SAMLBootstrap {
+
+	 @Override
+	    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+	        super.postProcessBeanFactory(beanFactory);
+	        BasicSecurityConfiguration config = (BasicSecurityConfiguration) Configuration.getGlobalSecurityConfiguration();
+	        config.registerSignatureAlgorithmURI("RSA", SignatureConstants.ALGO_ID_SIGNATURE_RSA_SHA256);
+	        config.setSignatureReferenceDigestMethod(SignatureConstants.ALGO_ID_DIGEST_SHA256);
+	    }
+}
