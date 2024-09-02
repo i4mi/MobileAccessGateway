59b3c2d7ed55e8b16bfd1e28f5611c6814deac96
Quentin Ligier <quentin.ligier@bluewin.ch>
2024-09-02 13:13:32 +0200

    #168 Allow Oauth2 login from any path

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FhirConfigService } from './fhirConfig.service';
 import { TranslateService } from '@ngx-translate/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,10 +11,22 @@ import { TranslateService } from '@ngx-translate/core';
 export class AppComponent {
   constructor(
     translateService: TranslateService,
-    fhirConfigService: FhirConfigService
+    fhirConfigService: FhirConfigService,
+    router: Router,
   ) {
     translateService.setDefaultLang('de');
     translateService.use(translateService.getBrowserLang());
+
+    // When the user loads the front page and there is an OAuth token in the URL, redirect to the Mag page
+    const urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.has('code') && urlParams.has('state')) {
+      router.navigate(['/mag'], {
+        queryParams: {
+          code: urlParams.get('code'),
+          state: urlParams.get('state'),
+        },
+      });
+    }
 
     let base = location.origin + location.pathname;
     if (base.endsWith('/')) {
